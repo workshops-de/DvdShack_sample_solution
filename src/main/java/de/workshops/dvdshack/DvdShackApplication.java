664d4104e2231e4838f6efc67f71091493a38200
Björn Wilmsmann <bjoern@bjoernkw.com>
2023-05-23 21:56:14 +0200

    Configure a PostgreSQL database connection

--- src/main/java/de/workshops/dvdshack/DvdShackApplication.java
+++ src/main/java/de/workshops/dvdshack/DvdShackApplication.java
@@ -1,13 +1,27 @@
 package de.workshops.dvdshack;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import javax.sql.DataSource;
+import java.sql.SQLException;
+
 @SpringBootApplication
-public class DvdShackApplication {
+public class DvdShackApplication implements CommandLineRunner {
+
+	@Autowired
+	private DataSource dataSource;
 
 	public static void main(String[] args) {
 		SpringApplication.run(DvdShackApplication.class, args);
 	}
 
+	@Override
+	public void run(String... args) throws SQLException {
+		System.out.println(
+				dataSource.getConnection().getMetaData().getURL()
+		);
+	}
 }
