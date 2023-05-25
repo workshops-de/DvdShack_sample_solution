1df12271e02720b9e6fb785d91f8b24079a2127b
Björn Wilmsmann <bjoern@bjoernkw.com>
2023-05-25 16:19:22 +0200

    Add a table by using Liquibase for migrations

--- src/main/resources/db/migration/V2__Create_loyalty_card_table.sql
+++
@@ -1,6 +0,0 @@
-CREATE TABLE loyalty_card (
-    id BIGINT PRIMARY KEY,
-    points INT NOT NULL DEFAULT 0,
-    customer_id BIGINT,
-    FOREIGN KEY (customer_id) REFERENCES customer(customer_id)
-);
