aa55ecf6f3ef4779c3f0e97c2d261f17db55b58b
Björn Wilmsmann <bjoern@bjoernkw.com>
2023-05-29 14:49:10 +0200

    Solution for task "Enrich your tests with fixtures"

--- src/test/java/de/workshops/dvdshack/repository/ActorJpaRepositoryTest.java
+++ src/test/java/de/workshops/dvdshack/repository/ActorJpaRepositoryTest.java
@@ -4,11 +4,13 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.test.context.jdbc.Sql;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 @DataJpaTest
 @AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
+@Sql(scripts={"/actors.sql"})
 class ActorJpaRepositoryTest {
     @Autowired
     ActorJpaRepository repository;
@@ -38,5 +40,14 @@ class ActorJpaRepositoryTest {
 
         actor = repository.findFirstAsActorByFirstNameAndLastName("CUBA", "ALLEN");
         assertThat(actor).isNotNull();
+    }
+
+    @Test
+    void shouldFindAdditionalActorsByLastName() {
+        final var actors = repository.findAllByFirstNameAndLastName("Mike", "Cox");
+        assertThat(actors)
+                .hasSize(1)
+                .extracting("lastName")
+                .containsOnly("Cox");
     }
  }
